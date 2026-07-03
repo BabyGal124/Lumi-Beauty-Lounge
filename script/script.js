c17f25d9a29f3f65412131a3f08b923b95ce6494
Esther <haryormideygold123@gmail.com>
2026-07-03 13:44:53 +0100

    updated index.html, updated style.css with media queries, udpated script.js

--- script/script.js
+++ script/script.js
@@ -9,3 +9,16 @@ appointmentForm.addEventListener('submit', function(e) {
     // Reset the form fields
     appointmentForm.reset();
 });
+
+// Navbar toggle
+const openBtn= document.getElementById('menu-open-button');
+const closeBtn= document.getElementById('menu-close-button');
+const navMenu= document.getElementById('nav-menu');
+
+openBtn.addEventListener('click', () => {
+    navMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+});
